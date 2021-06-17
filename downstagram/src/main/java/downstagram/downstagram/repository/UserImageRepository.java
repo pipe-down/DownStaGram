355002b3956ecaf6171d25b022c2f4358bc9af43
pipe-down <laljeongho@gmail.com>
2021-06-17 13:28:06 +0900

    정리

--- downstagram/src/main/java/downstagram/downstagram/repository/UserImageRepository.java
+++
@@ -1,8 +0,0 @@
-package downstagram.downstagram.repository;
-
-import downstagram.downstagram.domain.UserImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserImageRepository extends JpaRepository<UserImage, Long> {
-
-}
