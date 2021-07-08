6ced17f9ea07fa6158ce37b768d948b66e7158b8
pipe-down <laljeongho@gmail.com>
2021-07-08 16:09:05 +0900

    채팅방 프로토타입 완료

--- downstagram/src/main/java/downstagram/downstagram/controller/ChatController.java
+++ downstagram/src/main/java/downstagram/downstagram/controller/ChatController.java
@@ -1,13 +1,25 @@
 package downstagram.downstagram.controller;
 
+import downstagram.downstagram.model.UserDto;
+import downstagram.downstagram.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
 
+    private final UserService userService;
+
     @GetMapping("/chat/room")
-    public String chat(){
+    public String chat(Model model){
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        model.addAttribute("user", new UserDto(userService.findByUserId(userId)));
+
         return "/chat/chat";
     }
 }
