c83495f720b62830f433a8701488cd20f8d40cfa
pipe-down <laljeongho@gmail.com>
2021-07-17 15:37:34 +0900

    ChatRoomRepository 다시

--- downstagram/src/main/java/downstagram/downstagram/repository/ChatRoomRepository.java
+++ downstagram/src/main/java/downstagram/downstagram/repository/ChatRoomRepository.java
@@ -2,6 +2,15 @@ package downstagram.downstagram.repository;
 
 import downstagram.downstagram.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("select r from ChatRoom r order by r.roomId desc ")
+    List<ChatRoom> chatRoomList();
+
+    Optional<ChatRoom> findByRoomId(String roomId);
 }
