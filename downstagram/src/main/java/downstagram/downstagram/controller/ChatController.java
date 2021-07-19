8be56bf2d25dce4d758680db05f248b588b35ecc
pipe-down <laljeongho@gmail.com>
2021-07-19 19:01:25 +0900

    chat 컨트롤러

--- downstagram/src/main/java/downstagram/downstagram/controller/ChatController.java
+++ downstagram/src/main/java/downstagram/downstagram/controller/ChatController.java
@@ -1,25 +1,57 @@
 package downstagram.downstagram.controller;
 
+import downstagram.downstagram.domain.ChatRoom;
 import downstagram.downstagram.model.UserDto;
+import downstagram.downstagram.service.ChatRoomService;
 import downstagram.downstagram.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/chat")
 public class ChatController {
 
     private final UserService userService;
+    private final ChatRoomService chatRoomService;
 
-    @GetMapping("/chat/room")
+    // 채팅 리스트 화면
+    @GetMapping("/room")
     public String chat(Model model){
         String userId = SecurityContextHolder.getContext().getAuthentication().getName();
 
         model.addAttribute("user", new UserDto(userService.findByUserId(userId)));
 
         return "/chat/chat";
+    }
+
+    // 모든 채팅방 목록 반환
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoom> room() {
+        return chatRoomService.findAllRoom();
+    }
+    // 채팅방 생성
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatRoomService.save(name);
+    }
+    // 채팅방 입장 화면
+    @GetMapping("/room/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable Long roomId) {
+        model.addAttribute("roomId", roomId);
+        return "/chat/roomdetail";
+    }
+    // 특정 채팅방 조회
+    @GetMapping("/room/{roomId}")
+    @ResponseBody
+    public ChatRoom roomInfo(@PathVariable String roomId) {
+        return chatRoomService.findRoomById(roomId);
     }
 }
