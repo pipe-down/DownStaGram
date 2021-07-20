75b2396876156b09fd4d3366bbceb02bfb20e26b
pipe-down <laljeongho@gmail.com>
2021-07-20 16:42:44 +0900

    이름별

--- downstagram/src/main/java/downstagram/downstagram/repository/ChatRoomRepository.java
+++ downstagram/src/main/java/downstagram/downstagram/repository/ChatRoomRepository.java
@@ -13,4 +13,5 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     List<ChatRoom> chatRoomList();
 
     Optional<ChatRoom> findByRoomId(String roomId);
+    Optional<ChatRoom> findByName(String name);
 }
